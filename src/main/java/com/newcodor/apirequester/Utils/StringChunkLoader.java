0381ea272bdb385f7de5b67d00f604a03c65a9f4
newcodor <28708939+newcodor@users.noreply.github.com>
2025-02-14 00:32:23 +0800

    feature: add string split chunk util

---
+++ src/main/java/com/newcodor/apirequester/Utils/StringChunkLoader.java
@@ -0,0 +1,21 @@
+package com.newcodor.apirequester.Utils;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringChunkLoader {
+
+    public static List<String> splitIntoChunks(String text,int chunkSize) {
+        List<String> chunks = new ArrayList<>();
+        for (int i = 0; i < text.length(); i += chunkSize) {
+            int end = Math.min(i + chunkSize, text.length());
+//            System.out.println(end);
+            chunks.add(text.substring(i, end));
+        }
+        return chunks;
+    }
+}
+
+
